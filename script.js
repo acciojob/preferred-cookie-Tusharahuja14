de1f91eb7886cbfc1337b41e84e2fefa12b3785d
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-03-30 13:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let fsize=document.getElementById("fontsize");
+let fcolor=document.getElementById("fontcolor");
+let submit=document.getElementById("submit");
+submit.addEventListener("click",savecookies);
+function savecookies()
+{
+	document.cookie=`fontsize=${fsize.value};path=/`;
+	document.cookie=`fontcolor=${fcolor.value};path=/`;
+}
+window.addEventListener("load",()=>{
+	let cookies=document.cookie.split("; ").forEach(cookie=>{
+	let[key,value]=cookie.split("=");
+		if(key==="fontsize")
+		{
+			fsize.value=value;
+		}
+		if(key==="fontcolor")
+		{
+			fcolor.value=value;
+		}
+});
+});
